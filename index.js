b63b263dc7d6fc32cb0b33f089cc292ae4fcf18d
berger <robertberger.dev@gmail.com>
2023-01-17 17:23:54 -0500

    ran npm start

---
+++ index.js
@@ -0,0 +1,6 @@
+"use strict";
+let myName = "Bob";
+let color = "green";
+//template string example
+let statement = `Hello, my name is ${myName} and I like the color ${color}.`;
+console.log(statement);
