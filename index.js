2ecc08ba1edccc185c2741855407e474cd994202
berger <robertberger.dev@gmail.com>
2023-01-17 17:41:52 -0500

    ran npm start

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ for (let hero of superheroes) {
 for (let hero in superheroes) {
     console.log(hero);
 }
+let student;
+student = {
+    firstName: 'Bob',
+    lastName: 'Bobertson',
+    age: 25,
+    program: 'Computer Science'
+};
+console.log(student);
