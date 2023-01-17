87c858b1f41f55ede2c2cfc6784b978d3461cc48
berger <robertberger.dev@gmail.com>
2023-01-17 17:34:39 -0500

    updated example code

--- index.ts
+++ index.ts
@@ -1,6 +1,9 @@
-let myName: string = "Bob";
-let color: string = "green";
-//template string example
-let statement: string = `Hello, my name is ${myName} and I like the color ${color}.`
+let superheroes = ["Batman", "Superman", "Wonder Woman"];
 
-console.log(statement);
+for (let hero of superheroes){
+    console.log(hero);
+}
+
+for (let hero in superheroes){
+    console.log(hero);
+}
