3cedf5e527b07373064b1749ba02c823823f0e07
berger <robertberger.dev@gmail.com>
2023-01-17 17:21:32 -0500

    added example code to index.ts file

---
+++ index.ts
@@ -0,0 +1,6 @@
+let myName: string = "Bob";
+let color: string = "green";
+//template string example
+let statement: string = `Hello, my name is ${myName} and I like the color ${color}.`
+
+console.log(statement);
